83891c4ccd730ccd782e148c5a94972d38b8acff
Aastha <aasthaanand193@hotmail.com>
2023-05-11 23:49:26 +0530

    post display and user feature trial

---
+++ routes/users/users.js
@@ -0,0 +1,9 @@
+//users feature is separately introduced
+const express = require("express");
+const router = express.Router();
+// router.get("/getUser", (req, res) => {
+//   let { username, password } = req.body;
+//   //continue from here
+//   //validate username and password and then render
+// });
+module.exports = router;
--- server.js
+++ server.js
@@ -5,15 +5,23 @@ const path = require("path");
 const { MongoConnected } = require("./database/database");
 const hbs = require("hbs");
 const postRouter = require("./routes/posts/posts");
+const userRouter = require("./routes/users/users");
 //router
 //middleware functions
 
 app.set("view engine", "hbs");
 hbs.registerPartials(path.join(__dirname, "views", "partials"));
-app.use(express.static(path.join(__dirname, "public"))); //to load static files
+//to load static files
+app.use(express.static(path.join(__dirname, "public"))); //only stylesheet
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+//feature of users not included in this branch of the project
+// app.get("/", (req, res) => {
+//   //first page that is loaded
+//   res.render("login");
+// });
 app.use("/post", postRouter);
+app.use("/users", userRouter);
 
 //when a connection with mongodb server is established then launch the nodejs server
 MongoConnected().then(() => {
